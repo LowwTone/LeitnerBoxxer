fe0847c429ab22e7b3beae0c10695b081ce29475
Jochum van Weert <jochumtue@gmail.com>
2024-04-16 22:32:58 +0200

    Added function to plot array of numbers (up to 5) on correct position of Leitnergrid Added testdata (first 16 days of regular Leitnergrid

--- script.js
+++ script.js
@@ -4,17 +4,54 @@ function DrawLines(context, amountOfLines, lineHeight=50) {
         context.lineTo(790, 50 + i * lineHeight);
         context.stroke();
     }
-  }
-  
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  
-  //lines:
-  DrawLines(ctx, 8);
-  
-  ctx.font = "30px Arial";
-  ctx.fillStyle = 'blue';
-  //ctx.fillText("Hello qsWorld",10,50);
-  for (let i = 0; i < 10; i++) {
+}
+
+function DrawNumbers(data) {
+    for (let i=0; i<boxNumbers.length; i++) {
+
+        let x_position = 10 + i * 30; //offset position by index
     
-  } 
+        for(const number of boxNumbers[i]) {
+            switch (number) {
+                case 1:
+                    ctx.fillStyle = 'red';
+                    ctx.fillText("1", x_position, 390); //fix coordinates
+                    break;
+                case 2:
+                    ctx.fillStyle = 'orange';
+                    ctx.fillText("2", x_position, 340); //fix coordinates
+                    break;
+                case 3:
+                    ctx.fillStyle = 'yellow';
+                    ctx.fillText("3", x_position, 290); //fix coordinates
+                    break;
+                case 4:
+                    ctx.fillStyle = 'green';
+                    ctx.fillText("4", x_position, 240); //fix coordinates
+                    break;
+                case 5:
+                    ctx.fillStyle = 'blue';
+                    ctx.fillText("5", x_position ,190); //fix coordinates
+                    break;
+                //FIXME: add more cases
+            }
+        }
+    }
+}
+  
+//get reference to canvas context
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.font = "30px Arial";
+
+//lines:
+DrawLines(ctx, 8);
+
+//testdata, replace with generated algorithmic data later:
+var boxNumbers = [[1,2],[1,3],[1,2],[1,4],[1,2],[1,3],[1,2],[1],[1,2],[1,3],[1,2],[1,5],[1,2,4],[1,3],[1,2],[1]];
+
+//numbers:
+DrawNumbers(boxNumbers);
+
+
+
