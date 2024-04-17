c105da070d5d0a2ea9440156992511891076cec8
J van Weert <jochumtue@gmail.com>
2024-04-17 10:02:31 +0200

    added outline/stroke to numbers in chart for legibility

--- script.js
+++ script.js
@@ -10,27 +10,35 @@ function DrawNumbers(data) {
     for (let i=0; i<boxNumbers.length; i++) {
 
         let x_position = 10 + i * 30; //offset position by index
+		
+		ctx.lineWidth = 3;
+		ctx.strokeStyle = 'black';
     
         for(const number of boxNumbers[i]) {
             switch (number) {
-                case 1:
+                case 1:					
                     ctx.fillStyle = 'red';
+					ctx.strokeText("1", x_position, 390); //fix coordinates
                     ctx.fillText("1", x_position, 390); //fix coordinates
                     break;
                 case 2:
                     ctx.fillStyle = 'orange';
+                    ctx.strokeText("2", x_position, 340); //fix coordinates
                     ctx.fillText("2", x_position, 340); //fix coordinates
                     break;
                 case 3:
                     ctx.fillStyle = 'yellow';
+                    ctx.strokeText("3", x_position, 290); //fix coordinates
                     ctx.fillText("3", x_position, 290); //fix coordinates
                     break;
                 case 4:
                     ctx.fillStyle = 'green';
+                    ctx.strokeText("4", x_position, 240); //fix coordinates
                     ctx.fillText("4", x_position, 240); //fix coordinates
                     break;
                 case 5:
                     ctx.fillStyle = 'blue';
+                    ctx.strokeText("5", x_position ,190); //fix coordinates
                     ctx.fillText("5", x_position ,190); //fix coordinates
                     break;
                 //FIXME: add more cases
